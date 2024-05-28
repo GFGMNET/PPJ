ab40ae6c39901d0f732c24246d426d5ebdcae630
Your Name <you@example.com>
2024-05-28 08:20:54 +0200

    Automatic Sync

--- index.php
+++ index.php
@@ -5,20 +5,21 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PPJ</title>
-  <link rel="stylesheet" href="public/styles.css">
+  <link rel="stylesheet" href="./public/styles.css">
 </head>
 
 <body>
     <div class="wr">
-        <div class="left">
-        </div>     
-        <div class="right">
-            <div class="lblname">
-                Rechte
-            </div>
-        </div>
+       <div class="top">
+         menu
+       </div>
+       <form action="welcomeget.php" method="GET">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+        </form>
     </div>
-  <script src="public/scripts.js"></script>
+  <script src="./public/scripts.js"></script>
 </body>
 
 </html>
