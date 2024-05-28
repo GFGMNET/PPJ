3826b5fff7f82b8fcb73eef6c7e471b56a6c3a1b
Your Name <you@example.com>
2024-05-28 08:05:53 +0200

    Automatic Sync

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-h1 Welcome
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PPJ</title>
+  <link rel="stylesheet" href="public/styles.css">
+</head>
+
+<body>
+    <div class="wr">
+        <h1>Test</h1>
+    </div>
+  <script src="public/scripts.js"></script>
+</body>
+
+</html>
