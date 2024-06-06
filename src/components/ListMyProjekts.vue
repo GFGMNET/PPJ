00057248cc6241b6f34112ec0b5aef74ea83fe6e
Your Name <you@example.com>
2024-06-06 05:08:31 +0200

    Automatic Sync

--- src/components/ListMyProjekts.vue
+++ src/components/ListMyProjekts.vue
@@ -1,21 +1,13 @@
 <template>
-  <section>
+  <v-card>
     <form @submit.prevent="createPost">
-      <div>
-        <label for="userId">UserID:</label>
-        <input type="text" id="userId" v-model="postData.userId">
-      </div>
-      <div>
-        <label for="title">Title: </label>
-        <input type="text" id="title" v-model="postData.title">
-      </div>
-      <div>
-        <label for="body">Body: </label>
-        <textarea id="body" rows="6" cols="22" v-model="postData.body"></textarea>
-      </div>
+
+        <v-text-field label="UserID" type="text" id="userId" v-model="postData.userId"></v-text-field>
+        <v-text-field label="UserID" type="text" id="userId" v-model="postData.title"></v-text-field>
+        <v-text-field label="UserID" type="text" id="userId" v-model="postData.Body"></v-text-field>
       <button>Create Post</button>
     </form>
-  </section>
+  </v-card>
 </template>
 
 <script>
